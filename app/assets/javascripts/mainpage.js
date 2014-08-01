510e8997864c0372f1c48fc437a49f5d1ffbfa2b
bean <bean@f1lab.mobi>
2014-08-01 23:57:34 +0800

    finish date sidebar switch

---
+++ app/assets/javascripts/mainpage.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+	var currentMonth;
+	var MonthArray = $('.month');
+
+	initDateSlide();
+
+	$('.sidebar ul li a').click(function(e) {
+		e.preventDefault();
+	});
+
+	function initDateSlide() {
+		MonthArray.each(function() {
+			$(this).siblings().hide();
+			$(this).click(function() {
+				switchMonthSlide($(this));
+			})
+		});
+		currentMonth = $('.month').first();
+		openMonthSlide(currentMonth);
+
+
+	}
+
+	function closeMonthSlide(currentMonth) {
+		currentMonth.removeClass('currentMonth');
+		currentMonth.siblings().hide(800);
+	}
+
+	function openMonthSlide(currentMonth) {
+		currentMonth.addClass('currentMonth');
+		currentMonth.siblings().show(800);
+	}
+
+	function switchMonthSlide(currentMonth) {
+		if (!currentMonth.hasClass('currentMonth')) {
+			MonthArray.each(function() {
+				if ($(this).hasClass('currentMonth')) {
+					closeMonthSlide($(this));
+				}
+			});
+		} 
+		if (currentMonth.hasClass('currentMonth')) {
+			closeMonthSlide(currentMonth);
+		} else {
+			openMonthSlide(currentMonth);
+		}
+
+	}
+});
