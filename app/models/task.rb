9386db71a260358d5f80bd24f3a4e5b6bb3e2419
gechentuo <200893255@qq.com>
2014-09-02 13:17:41 +0800

    reject blank record saved

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ActiveRecord::Base
 	belongs_to :day
 	has_many :records
-	accepts_nested_attributes_for :records, :allow_destroy => true
+	accepts_nested_attributes_for :records, allow_destroy: true,reject_if: :all_blank
 end
